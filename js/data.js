4bba18801be4d832fb84a152abd008234a0cbee5
Aug <heyaugust@gmail.com>
2014-12-14 01:58:09 +0800

    added more values for data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,46 @@
 var data = [
-	{name:'Zhou Mi', category:'Nature', age:'26', age_index:'2', gender:'Female', profilePic:'../img/zmprofile.jpg'},
-	{name:'Melissa', category:'Food', age:'26', age_index:'2', gender:'Female', profilePic:'../img/melprofile2.jpg'},
-	{name:'Gary', category:'Culture', age:'25', age_index:'1', gender:'Male', profilePic:'../img/Garyprofile.jpg'},
-	{name:'Tommy', category:'Nature', age:'35', age_index:'3', gender:'Male', profilePic:'../img/CLprofile.jpg'},
+	{
+		name:'Zhou Mi', 
+		category:'Nature', 
+		age:'26', 
+		age_index:'2', 
+		gender:'Female', 
+		profilePic:'../img/zmprofile.jpg', 
+		bio: "Hello everyone! Join me and exercise in Singapore.",
+		like: "10",
+		tour: "3"
+	},
+	{
+		name:'Melissa', 
+		category:'Food', 
+		age:'26', 
+		age_index:'2', 
+		gender:'Female', 
+		profilePic:'../img/melprofile2.jpg', 
+		bio: "Hi I'm Melissa! I am a foodie!",
+		like: "5",
+		tour: "2"
+	},
+	{
+		name:'Gary', 
+		category:'Culture', 
+		age:'25', 
+		age_index:'1', 
+		gender:'Male', 
+		profilePic:'../img/Garyprofile.jpg', 
+		bio: "Yo I am Gary. I love to play video games and hang out with new friends.",
+		like: "3",
+		tour: "1"
+	},
+	{
+		name:'Tommy', 
+		category:'Nature', 
+		age:'35', 
+		age_index:'3', 
+		gender:'Male', 
+		profilePic:'../img/CLprofile.jpg', 
+		bio:"Wassup!! Welcome to Singapore!",
+		like: "8",
+		tour: "5"
+	},
 ];
