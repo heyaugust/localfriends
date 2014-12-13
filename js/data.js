45a7c7c1567d21e17b63a9eed6c8ccfb6328b057
Aug <heyaugust@gmail.com>
2014-12-13 15:45:28 +0800

    added data.js, js for selection

---
+++ js/data.js
@@ -0,0 +1,6 @@
+var data = [
+	{name:'Zhou Mi', category:'Nature', age:'26', age_index:'2', gender:'Female', profilePic:'../img/zmprofile.jpg'},
+	{name:'Melissa', category:'Food', age:'26', age_index:'2', gender:'Female', profilePic:'../img/melprofile2.jpg'},
+	{name:'Gary', category:'Culture', age:'25', age_index:'1', gender:'Male', profilePic:'../img/Garyprofile.jpg'},
+	{name:'Tommy', category:'Nature', age:'35', age_index:'3', gender:'Male', profilePic:'../img/CLprofile.jpg'},
+];
