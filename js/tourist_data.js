c5fe17221aff847c8bdcee3a6373ce04f06c66ba
Aug <heyaugust@gmail.com>
2014-12-14 10:08:39 +0800

    added js to notification messages buttons

---
+++ js/tourist_data.js
@@ -0,0 +1,13 @@
+var data = [
+	{
+		name:'Steve', 
+		category:'Nature', 
+		age:'31', 
+		age_index:'2', 
+		gender:'Male', 
+		profilePic:'../img/steveprofile.jpg', 
+		msgToMel: "Hi Melissa, I would like to explore Singapore. Where would be a good place to go?",
+		like: "10",
+		tour: "3"
+	}
+]
