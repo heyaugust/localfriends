090bbad28ae0fe04f3890b4df584cc086ffebab0
Aug <heyaugust@gmail.com>
2014-12-14 10:54:02 +0800

    added web socket

---
+++ js/websocket.js
@@ -0,0 +1,23 @@
+//create a WebSocket instance and start connecting to the server
+		var client = new WebSocket('ws://localhost:3001');
+
+		//when connected
+		client.onopen = function(){
+			console.log('connected');
+		}
+
+		//when message is received from the server
+		client.onmessage = function(event){
+			console.log('receive:', event.data);
+		}
+
+		client.onclose = function(){
+			console.warn('disconnected');
+		}
+
+function replyMsg() {
+	var hr = document.createElement("hr");
+	sendBtn.setAttribute("style", "display:none");
+	input.setAttribute("style", "display:none");
+	objTo.appendChild(hr);
+}
